7c62d5fa14259b2d22dc96b55545bbc2224c97ea
Florian Limberger <flo@purplekraken.com>
2021-09-02 16:46:18 +0200

    Fill EngineerActivity with a list of main activities

--- app/src/main/java/com/sirrgb/launcher/EngineerActivity.kt
+++ app/src/main/java/com/sirrgb/launcher/EngineerActivity.kt
@@ -1,39 +1,34 @@
 package com.sirrgb.launcher
 
-import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.content.pm.ResolveInfo
+import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class EngineerActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_engineer)
-		//MESSAGE getIntent().getStringExtra("key")
-		//var extras = intent.getIntExtra()
-		val globalinput = intent.getStringExtra("test")
-		val fibonacciArray = intent.getIntegerArrayListExtra("ALL_THE_FIBONACCIS")
-		val random = Math.floor(Math.random() * 42069) + 1
 
-
-		// Array Adapter
-        val arrayAdapter: ArrayAdapter<*>
-        val users = arrayListOf(
-            globalinput, "Rohit Sharma", "Steve Smith",
-            "Kane Williamson", "Ross Taylor", random
-        )
-		fibonacciArray!!.forEach { fibonacci ->
-			if(fibonacci >= 2) {
-				users.add(fibonacci)
-			}
-
+		val allMainActivities = getAllMainActivities()
+		allMainActivities.forEach {
+			val label = it.loadLabel(packageManager)
+			println(label)
 		}
-		// Array A
-
-        var mListView = findViewById<ListView>(R.id.listView)
-		arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+		val labels = allMainActivities.map {
+			it.loadLabel(packageManager)
+		}
+        val arrayAdapter: ArrayAdapter<*> = ArrayAdapter(this, android.R.layout.simple_list_item_1, labels)
+		val mListView = findViewById<ListView>(R.id.listView)
 		mListView.adapter = arrayAdapter
+	}
+
+
+	private fun getAllMainActivities(): List<ResolveInfo> {
+		val intent = Intent(Intent.ACTION_MAIN)
+		intent.addCategory(Intent.CATEGORY_LAUNCHER)
+		return packageManager.queryIntentActivities(intent, 0)
 	}
 }
